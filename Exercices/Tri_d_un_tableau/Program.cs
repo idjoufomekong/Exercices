e8bb853b8e624321f3948399688797a1751c692d
idjoufomekong <idjoufomekong@isagri.fr>
2017-03-22 16:11:20 +0100

    Tri d'un tableau

---
+++ Exercices/Tri_d_un_tableau/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tri_d_un_tableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tab = { "seize", "trois", "douze", "un" };//{ "vert","bleu", "rouge", "jaune"}; //;
+            AffichTab(tab);
+
+            TrieTableau(tab);
+            AffichTab(tab);
+
+            Console.ReadKey();
+
+
+        }
+
+        static void AffichTab(string[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+                Console.Write(tab[i] + "; ");
+        }
+
+        static void TrieTableau(string[] tab)
+        {
+            string perm;
+            bool auMoinsUnePermut = true;
+
+            //while (auMoinsUnePermut)
+            //{
+            //    auMoinsUnePermut = false;
+            //    for (int i = 0; i < tab.Length - 1; i++)
+            //    {
+
+            //        if (tab[i].CompareTo(tab[i + 1]) > 0)
+            //        {
+            //            perm = tab[i];
+            //            tab[i] = tab[i + 1];
+            //            tab[i + 1] = perm;
+            //            auMoinsUnePermut = true;
+            //        }
+
+
+            //    }
+
+            //}
+
+            for (int i = 0; i < tab.Length - 1; i++)
+            {
+                for (int j = i; j < tab.Length - 1; j++)
+                {
+                    if (tab[j].CompareTo(tab[j + 1]) > 0)
+                    {
+                        perm = tab[j];
+                        tab[j] = tab[j + 1];
+                        tab[j + 1] = perm;
+                        //auMoinsUnePermut = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
