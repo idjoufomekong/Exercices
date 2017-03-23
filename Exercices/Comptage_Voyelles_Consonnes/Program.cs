75be96bb30dabec80d9c5e8fac028e958f4e9153
idjoufomekong <idjoufomekong@isagri.fr>
2017-03-23 17:12:13 +0100

    23Mars

---
+++ Exercices/Comptage_Voyelles_Consonnes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comptage_Voyelles_Consonnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string motsaisi;
+            int voy, cons;
+
+            Console.WriteLine("Saisir un mot: ");
+            motsaisi = Console.ReadLine();
+
+            Comptage_Voyelles_Consonnes(motsaisi, out voy, out cons);
+            //Console.WriteLine(motsaisi + "comprend" + cons + "consonnes et " +voy +"voyelles");
+            Console.WriteLine("{0} comprend {1} voyelles et {2} consonnes.",motsaisi,voy,cons);
+            Console.ReadKey();
+
+        }
+
+        static void Comptage_Voyelles_Consonnes(string mot, out int v, out int c)
+        {
+           // string chaine = mot;
+            int nbCar = mot.Length;
+            char car;
+            v = 0;
+
+            //Console.WriteLine("char:"+ chaine.ElementAt(0));
+            //Console.WriteLine("char:" + chaine.ElementAt(mot.Length -1));
+            for(int i=0; i<mot.Length;i++)
+            {
+                //car = mot.ElementAt(i);
+                car = mot[i];
+                //if ((car == 'a') || (car == 'e') || (car == 'i') || (car == 'o') || (car == 'u') || (car == 'y'))
+                //    v++;
+                switch (car)
+                {
+                    case 'a': case 'e': case 'i': case 'o': case 'u': case 'y':
+                        v++;
+                break;
+                
+            }
+
+        }
+            c = nbCar-v;
+            
+
+           
+        }
+    }
+}
---
+++ Exercices/Login/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login, mdp;
+            bool resultLogin;
+
+            Console.WriteLine("Saisir login: ");
+            login = Console.ReadLine();
+
+            try
+            {
+             do resultLogin=VerifierLogin(login);
+                while (resultLogin == false) ; 
+            }
+            catch (FormatException e)
+            {
+               Console.WriteLine(e.Message);
+            }
+           
+                   
+            Console.WriteLine("Saisir le mot de passe: ");
+            mdp = Console.ReadLine();
+            
+            try
+            {
+                VerifierMdp(mdp);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+            Console.WriteLine("Votre compte a bien été créé. Un message vient de vous être envoyé");
+            Console.ReadKey();
+            
+        }
+
+        static bool VerifierLogin(string mot)
+        {
+            bool result = true;
+            if (mot.Length < 5)
+            {
+                result = false;
+                throw new FormatException("Taille insuffisante du login");
+            }
+            return result;
+
+        }
+
+        static void VerifierMdp(string mdp)
+        {
+            if((mdp.Length<6)|| (mdp.Length >12)|| (mdp[0]==' ') || (mdp[mdp.Length-1] == ' '))
+                throw new FormatException("Le mot de passe ne respecte pas les conditions");
+        }
+
+    }
+}
