d86f6e506fc7a509d757e55bc47a480b3261bbf6
idjoufomekong <idjoufomekong@isagri.fr>
2017-03-28 17:19:30 +0200

    28 Mars

---
+++ Exercices/Vehicules/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicules
+{
+    public enum Energies { Aucune, Essence, Gazole, GPL, Electrique}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture v1 = new Voiture("Merco", Energies.Gazole);
+            Console.WriteLine(v1.Description);
+
+            Véhicule v2 = new Moto("Cross", Energies.Essence);
+            Véhicule v3 = new Voiture("Renault", Energies.Essence);
+            Console.WriteLine(v2.Description);
+            Console.WriteLine(v3.Description);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercices/Vehicules/Véhicule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicules
+{
+    public class Véhicule
+    {
+        #region Propriétés             
+        public string Nom { get; set; }
+        public int NbRoues { get; set; }
+        public Energies Energie { get; set; }
+        public virtual string Description
+        {
+            get
+            {
+                return string.Format("Véhicule {0} roule sur {1} roues et à l'énergie {2}.", Nom, NbRoues, Energie);
+            }
+        }
+        #endregion
+
+        #region Constructeurs
+        public Véhicule(string nom, int nbRoues,Energies energie)
+        {
+            Nom = nom;
+            this.NbRoues = nbRoues;
+            Energie = energie;
+        }
+        #endregion
+
+        #region Méthodes publiques
+        public override string ToString()
+        {
+            return string.Format("Je suis un véhicule {0}. ", Energie );
+        }
+        #endregion
+    }
+
+    public class Voiture : Véhicule
+    {
+        private const int NB_ROUES_VOITURE = 4;
+
+        #region Propriétés
+        public override string Description
+        {
+            get
+            {
+                return "Je suis une voiture.\r\n" + base.Description;
+            }
+        }
+        #endregion
+        public Voiture(string nom, Energies energie): base(nom, NB_ROUES_VOITURE, energie)
+        {
+
+        }
+
+        #region Méthodes publiques
+        public override string ToString()
+        {
+            return base.ToString() + string.Format("Je suis une voiture car j'ai {0} roues.",NbRoues);
+        }
+
+        #endregion
+    }
+
+    public class Moto : Véhicule
+    {
+        private const int NB_ROUES_MOTO = 2;
+
+        #region Propriétés
+        public override string Description
+        {
+            get
+            {
+                return "Je suis une moto.\r\n" + base.Description;
+            }
+        }
+        #endregion
+        public Moto(string nom, Energies energie) : base(nom, NB_ROUES_MOTO, energie)
+        {
+
+        }
+
+    }
+}
