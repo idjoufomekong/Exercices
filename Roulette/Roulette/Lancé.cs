79f99ba214f1f76c41d5a56d000db0ae3916e2a2
idjoufomekong <idjoufomekong@isagri.fr>
2017-04-12 15:12:33 +0200

    gg

---
+++ Roulette/Roulette/Lancé.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    public class Lancé
+    {
+        #region Propriétés
+        public int? Numéro { get; }
+        public Combinaisons Combinaison { get; }
+        #endregion
+
+        #region Constructeur
+        public Lancé(int? num, Combinaisons combi)
+        {
+            Numéro = num;
+            Combinaison = combi;
+        }
+        #endregion
+
+        #region Méthodes publiques
+        public bool CorrespondA(Lancé lance)
+        {
+            if(lance.Combinaison != Combinaisons.Précis)
+            {
+                if (lance.Combinaison == Combinaison)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                if (lance.Numéro == Numéro)
+                    return true;
+                else
+                    return false;
+            }
+            
+        }
+        #endregion
+    }
+}
---
+++ Roulette/Roulette/Mise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roulette
+{
+    [Flags]
+    public enum Combinaisons {
+    Aucun = 0,
+    Premiers24 = 1,
+    Derniers24 = 2,
+    Rouge = 4,
+    Noir = 8,
+    Impair = 32,
+    Pair = 64,
+    Précis = 128
+    }
+    class Mise
+    {
+    }
+}
