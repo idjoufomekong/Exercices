8705b150cc149c5e134b0d9cb18e0526984b8544
djou <idjoufomekong@isagri.fr>
2017-04-14 00:20:12 +0200

    dd

--- Roulette/Roulette/Lancé.cs
+++ Roulette/Roulette/Lancé.cs
@@ -24,6 +24,62 @@ namespace Roulette
         #region Méthodes publiques
         public bool CorrespondA(Lancé lance)
         {
+            if (lance.Combinaison == Combinaisons.Précis)
+            {
+                if (lance.Numéro == Numéro)
+                    return true;
+                else
+                    return false;
+            }
+            else
+            {
+                switch (lance.Combinaison)
+                {
+                    case Combinaisons.Derniers24:
+                        List<int> listeDerniers24 = new List<int>();
+                        for (int i = 13; i <= 36; i++)
+                        {
+                            listeDerniers24.Add(i);
+                        }
+                        if (listeDerniers24.Contains((int)lance.Numéro))
+                            return true;
+                        else
+                            return false;
+                    case Combinaisons.Premiers24:
+                        List<int> listePremiers24 = new List<int>();
+                        for (int i = 1; i <= 24; i++)
+                        {
+                            listePremiers24.Add(i);
+                        }
+                        if (listePremiers24.Contains((int)lance.Numéro))
+                            return true;
+                        else
+                            return false;
+                    case Combinaisons.Noir:
+                        int[] listeNoir = { 2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
+                       
+                        if (Array.BinarySearch(listeNoir,lance.Numéro) < 0)
+                            return true;
+                        else
+                            return false;
+                    case Combinaisons.Rouge:
+                        int[] listeRouge = { 1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36 };
+
+                        if (Array.BinarySearch(listeRouge, lance.Numéro) < 0)
+                            return true;
+                        else
+                            return false;
+                    case Combinaisons.Impair:
+                        if (lance.Numéro % 2 != 0)
+                            return true;
+                        else return false;
+                    case Combinaisons.Pair:
+                        if (lance.Numéro % 2 == 0)
+                            return true;
+                        else return false;
+                }
+
+            }
             return true;
         }
         #endregion
